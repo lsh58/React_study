c611a9e1ee42cbd47005bcb5b244fc68def29406
lsh58 <lsh58@gmail.com>
2020-06-18 12:23:07 +0900

    finish

--- 생활코딩/react-app/src/App.js
+++ 생활코딩/react-app/src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import TOC from "./components/TOC"
-import Content from "./components/Content"
+import ReadContent from "./components/ReadContent"
+import CreateContent from "./components/CreateContent"
+import UpdateContent from "./components/UpdateContent"
 import Subject from "./components/Subject"
+import Control from "./components/Control"
 import './App.css';
 
 class App extends Component {
   constructor(props){ //초기화담당
     super(props);
+    this.max_content_id=3;
     this.state = {
       mode:"welcome",
       subject:{ title:"WEB", sub:"world wide web!"},
@@ -18,24 +22,63 @@ class App extends Component {
       ]
     }
   }
-  render() {
-    var _title, _desc = null;
+  getReadContent(){
+    var i=0;
+    while(i < this.state.contents.length){
+      var data= this.state.contents[i];
+      if(data.id === this.state.selected_content_id){
+        return data;
+        break;
+      }
+      i = i+1;
+    }
+  }
+  getContent(){
+    var _title, _desc, _article = null;
     if(this.state.mode === "welcome"){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }
     else if(this.state.mode === "read"){
-      var i=0;
-      while(i < this.state.contents.length){
-        var data= this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content._title} desc={_content._desc}></ReadContent>
+    }else if(this.state.mode === 'create'){
+      _article =<CreateContent onSubmit={function(_title,_desc){
+        this.max_content_id=this.max_content_id+1;
+        var _contents = this.state.contents.concat({
+          id:this.max_content_id,
+          title:_title,
+          desc:_desc
+        })
+        this.setState({
+          contents:_contents,
+          mode:'read',
+          selected_content_id:this.max_content_id
+        });
+      }.bind(this)}></CreateContent>
+    } else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article =<UpdateContent data={_content} 
+      onSubmit={function(_id, _title, _desc){
+        var _contents = Array.from(this.state.contents);
+        var i=0;
+        while(i<_contents.length){
+          if(_contents[i].id === _id) {
+            _contents[i] = {id:_id, title:_title, desc:_desc};
+            break;
+          }
+          i=i+1;
         }
-        i = i+1;
-      }
+        this.setState({
+          contents:_contents,
+          mode:'read'
+        });
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
     return (
       <div className="App">
           <Subject 
@@ -53,7 +96,30 @@ class App extends Component {
               });
             }.bind(this)}>
             </TOC>
-          <Content title={_title} desc={_desc}></Content>
+            <Control onChangeMode={function(_mode){
+              if(_mode==='delete'){
+                if(window.confirm()){
+                  var _contents = Array.from(this.state.contents);
+                  var i=0;
+                  while(i<this.state.contents.length){
+                    if(_contents[i].id === this.state.selected_content_id){
+                      _contents.splice(i,1);
+                      break;
+                    }
+                    i=i+1;
+                  }
+                  this.setState({
+                    mode:'welcome',
+                    contents:_contents
+                  })
+                }
+              }else{
+                this.setState({
+                mode:_mode
+              });
+              }
+            }.bind(this)}></Control>
+          {this.getContent()}
       </div>
     );
   }
--- 생활코딩/react-app/src/components/ReadContent.js
+++ 생활코딩/react-app/src/components/ReadContent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-class Content extends Component{
+class ReadContent extends Component{
     render(){
       return(
         <article>
@@ -11,5 +11,5 @@ class Content extends Component{
     }
   }
 
-  export default Content;
+  export default ReadContent;
   
